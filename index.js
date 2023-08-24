556a1a73e42b375012a1c10ae11caf7c07ad0d6a
Shippo704 <matt.taylor704@yahoo.ca>
2023-08-24 00:05:32 -0400

    added svg class to index.js

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
-//Required packages
+//Required packages and classes
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateSvg = require('generate-svg');
+const {Circle, Triangle, Square} = require('./lib/shapes');
 
-// array of questions for generating logo
+class SVG {
+    constructor() {
+        this.text = '';
+        this.shape = '';
+    }
+    // Render the logo
+    render() {
+        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" 
+            width="300" 
+            height="300"
+            >
+            ${this.shape}${this.text}</svg>`
+    }
+
+    // Set the text
+    setText(text, colour) {
+        this.text = `<text 
+            x="150" 
+            y="200" 
+            font-size="72" 
+            text-anchor="middle" 
+            fill="${colour}"
+            >
+            ${text}
+            </text>`
+    }
+
+    // Set the shape
+    setShape(shape) {
+        this.shape = shape.render();
+    }
+}
+
+// Array of questions for generating logo
 const questions = [
     //Text
     {
@@ -38,3 +71,8 @@ const questions = [
         message: 'Enter a colour for your shape. Input either a colour name or a hexadecimal code in the format #RRGGBB: '
     }
 ]
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => 
+        err ? console.error(err) : console.log("Logo created successfully"));
+}
